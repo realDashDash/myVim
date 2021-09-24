7f9b44eb60d7e713cf89298ab6e5218165ecdfa9
Zhang Dnaxu <dzhang022@e.ntu.edu.sg>
2021-09-24 15:45:13 +0000

    Change directory

--- basic_install.sh
+++ basic_install.sh
@@ -5,6 +5,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
-cat basic.vim > ~/.vimrc
+cat config/basic.vim > ~/.vimrc
 
 echo "Installed basic Vim configuration!"
--- extra_install.sh
+++ extra_install.sh
@@ -4,7 +4,7 @@ set -e
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cat basic.vim plug.vim > ~/.vimrc
+cat config/basic.vim plug.vim > ~/.vimrc
 
 echo "Installed vim configurations with plug-ins. "
 echo "You can modify your plug-in in ~/.vimrc"
